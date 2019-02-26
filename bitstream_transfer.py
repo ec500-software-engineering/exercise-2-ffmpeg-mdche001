e2e1819edf231fc3233614e0ae8082a4007407c2
mdche001 <mdche@bu.edu>
2019-02-26 09:48:36 +0800

    improve

--- bitstream_transfer.py
+++ bitstream_transfer.py
@@ -2,21 +2,40 @@ import ffmpy
 import os
 import subprocess
 import queue
-import  threading
+import threading
+import asyncio
+import pytest
+import time
 
-def transfer_bit(queue_name):
+async def transfer_bit480(queue_name):
     "Convert vedio"
     transfer_file = queue_name.get()
-    print(transfer_file)
+    # print(transfer_file)
     try:
         # os.chdir('C:/Users/synox/mini_project1/') change to dirctionary including images
-        subprocess.Popen("ffmpeg -i ./" + transfer_file + " -vcodec h264 -b:v 1048576 -s 720*480 -r 30 ./transcoded_video.mp4")
-        # subprocess.call(["ffmpeg -y -i" +"/Makefile.mp4" + "- vcodec h264 - b:v 1048576 - s 720*480 - r 30" + "/transcoded_video.mp4"])
-        print("All done")
+        # oppo = ['ffmpeg -i' + transfer_file + '-r 30 -s hd480 -b:v 1024k -loglevel quiet' + t./transcoded_video.mp4]
+        subprocess.Popen('ffmpeg -i ./' + transfer_file + ' -r 30 -s hd480 -b:v 1024k ./transcoded_video_480p.mp4')
+        # subprocess.Popen("ffmpeg -i ./" + transfer_file + " -vcodec h264 -b:v 1048576 -s 720*480 -r 30 ./transcoded_video.mp4")
+        print("--------------480p All done")
+        return "transcoded_video_480p"
+
     except:
         print('error')
 
-def put_in_queue(queue_name,file_name):
+
+async def transfer_bit720(queue_name):
+    "Convert vedio"
+    transfer_file = queue_name.get()
+    # print(transfer_file)
+    try:
+        subprocess.Popen('ffmpeg -i ./' + transfer_file + ' -r 30 -s hd720 -b:v 1024k ./transcoded_video_720p.mp4')
+        print("-------------720p All done")
+        return "transcoded_video_720p"
+
+    except:
+        print('error')
+
+async def put_in_queue(queue_name,file_name):
     format_name = file_name + ".mp4"
     if (queue_name.full()== True):
         queue_name.get()
@@ -24,15 +43,26 @@ def put_in_queue(queue_name,file_name):
     else:
         queue_name.put(format_name)
 
-def ffmpeg_combine(queue_name):
-    while (True):
-        file_name = input("Plz input your the name of your media files:\n")
-        put_in_queue(queue_name, file_name)
-        transfer_bit(queue_name)
 
 if __name__ == "__main__":
+    start_time = time.clock()
     q = queue.Queue()
-    for i in range(5):
-        t= threading.Thread(target=ffmpeg_combine(q))
-        t.start()
-    t.join()
+
+    file_name = input("Plz input your the name of your media files:\n")
+    if (file_name):
+        loop = asyncio.get_event_loop()
+        task = [asyncio.ensure_future(put_in_queue(q, file_name)),asyncio.ensure_future(transfer_bit480(q)), asyncio.ensure_future(transfer_bit720(q))]
+        loop.run_until_complete(asyncio.wait(task))
+
+    files = os.listdir()
+    for file in files:
+        if(os.path.basename(url) == "ranscoded_video_480p.mp4"):
+            file_name1 == os.path.basename(url)
+        elif(os.path.basename(url) == "ranscoded_video_720p.mp4"):
+            file_name2 == os.path.basename(url)
+
+    assert filename1 == "ranscoded_video_480p.mp4"
+    assert filename2 == "ranscoded_video_720p.mp4"
+    print("---transfermation successful!--------")
+    duration = time.clock() - start_time
+    print("Total_running time is:", duration)
